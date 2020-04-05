e19742ebf4ec8f169abcec17e6357d4e659d194d
MSidda <42439015+MSidda@users.noreply.github.com>
2020-04-05 23:52:33 +0530

    Update main.tf

--- Terraform/modules/keyvault/main.tf
+++ Terraform/modules/keyvault/main.tf
@@ -10,8 +10,8 @@ data "azurerm_client_config" "current" {
 
 resource "azurerm_key_vault" "example" {
   name                        = "testaPPs"
-  location                    = "${azurerm_resource_group.resource_group.location}"
-  resource_group_name         = "${azurerm_resource_group.resource_group.name}"
+  location                    = "${data.azurerm_resource_group.resource_group.location}"
+  resource_group_name         = "${data.azurerm_resource_group.resource_group.name}"
   enabled_for_disk_encryption = true
   tenant_id                   = "${data.azurerm_client_config.current.tenant_id}"
   soft_delete_enabled         = true
