b6339c05b9da926bd6ef93374f0fbcded3245852
MSidda <42439015+MSidda@users.noreply.github.com>
2020-04-06 20:16:47 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "subscription_id" {
-  default = "976709cf-14cb-4d18-a8c1-68a602b93aed"
+  default = ""
 }
 
 variable "client_id" {
-  default = "b850400c-a13f-4208-9f84-e1deb8086dd4"
+  default = ""
 }
 
 variable "client_secret" {
-  default = "b9d0e20f-b1fa-4d4c-99ac-57f726f43804"
+  default = ""
 }
 
 variable "tenant_id" {
-  default = "902eab19-c66d-43b3-91e5-d4c00ec64e88"
+  default = ""
 }
 
 # variable "aks_client_id" {
