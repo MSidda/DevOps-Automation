731bd7d00260dbc6549b32d0f2920e71bc58b24f
MSidda <42439015+MSidda@users.noreply.github.com>
2020-04-06 20:21:41 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -14,22 +14,12 @@ variable "tenant_id" {
   default = ""
 }
 
-# variable "aks_client_id" {
-#   default     = "53cefe4d-2e03-43d3-b7bd-df73ff529484"
-#   description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster"
-# }
-
-# variable "aks_client_secret" {
-#   default     = "hC_21iL2t5K-GfhvWX?R.a[XHUy39GZ?"
-#   description = "The Client Secret for the Service Principal to use for this Managed Kubernetes Cluster"
-# }
-
 variable "agent_count" {
   default     = 2
   description = "Number of worker nodes to be provisioned"
 }
 variable "username" {
-  default = "qldtfuser"
+  default = "user"
 }
 variable "ssh_public_key" {
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEArlTF4iXqJrYLXMHBfMmVuQ2h0vShblXOMCgoRucZserZSa1oWv8n7qbZPDctf/iH8ALI8O9wh07+qECj/PMemMF0jqMJgzHzPUmew9n6wraoP+EmEySgYpwVlg74ViykHR6lmbnGsvb5eNC+0pEyztUoyRydaGgNjsw82HXLXAyEASOaxfx46daW2vf+ZmsjgEa+yyfHdNyTFO1rZk7TapN7xtUilb0raEpFAHHdHOFVeYZvZqg3JifYd4bDKR5bDx9+4c+YUkikXuEvI1PMPpNJZUfgROyQESLFRLlJ7IQ5ChuT7ALgOCZTbG63CUWwAuK9nomyQrEUV5p/SHgNgw== rsa-key-20190129"
