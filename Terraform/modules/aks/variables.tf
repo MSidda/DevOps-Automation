9ce931801b08acd2c4a98e8e660162b950bba688
MSidda <42439015+MSidda@users.noreply.github.com>
2020-04-06 20:20:49 +0530

    Update variables.tf

--- Terraform/modules/aks/variables.tf
+++ Terraform/modules/aks/variables.tf
@@ -3,7 +3,7 @@ variable "agent_count" {
   description = "Number of worker nodes to be provisioned"
 }
 variable "username" {
-  default = "qldtfuser"
+  default = "user"
 }
 variable "ssh_public_key" {
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEArlTF4iXqJrYLXMHBfMmVuQ2h0vShblXOMCgoRucZserZSa1oWv8n7qbZPDctf/iH8ALI8O9wh07+qECj/PMemMF0jqMJgzHzPUmew9n6wraoP+EmEySgYpwVlg74ViykHR6lmbnGsvb5eNC+0pEyztUoyRydaGgNjsw82HXLXAyEASOaxfx46daW2vf+ZmsjgEa+yyfHdNyTFO1rZk7TapN7xtUilb0raEpFAHHdHOFVeYZvZqg3JifYd4bDKR5bDx9+4c+YUkikXuEvI1PMPpNJZUfgROyQESLFRLlJ7IQ5ChuT7ALgOCZTbG63CUWwAuK9nomyQrEUV5p/SHgNgw== rsa-key-20190129"
